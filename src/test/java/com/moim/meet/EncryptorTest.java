af75c47c09c9ae0df4e9c3e424dbc13a9f8998d6
cdssw <cdssw@DESKTOP-UKB1RQC>
2020-06-06 11:51:45 +0900

    DB 정보 암호화 처리

---
+++ src/test/java/com/moim/meet/EncryptorTest.java
@@ -0,0 +1,38 @@
+package com.moim.meet;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * EncryptorTest.java
+ * 
+ * @author cdssw
+ * @since 2020. 6. 6.
+ * @description  
+ * 
+ * <pre>
+ * since          author           description
+ * ===========    =============    ===========================
+ * 2020. 6. 6.    cdssw            최초 생성
+ * </pre>
+ */
+@Slf4j
+public class EncryptorTest {
+
+	@Test
+	public void encryptor() {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setAlgorithm("PBEWithMD5AndDES");
+		encryptor.setPassword("SecretKeySalt");
+		
+		String url = "jdbc:mariadb://{url}/{db}";
+		String username = "{id}";
+		String password = "{password}";
+		
+		log.info(encryptor.encrypt(url));
+		log.info(encryptor.encrypt(username));
+		log.info(encryptor.encrypt(password));
+	}
+}
